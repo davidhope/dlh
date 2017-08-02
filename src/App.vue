626e38036e5f7ab1a70fe769d2698d067aa252a7
davidhope <flav.flavor@gmail.com>
2017-08-02 00:34:39 -0500

    incorporating vue templates

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,13 @@
 <template>
-  <div id="app">
-    <img src="assets/img/logo/repute-logo-nav-slategray.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div>
+      <div class="heading-center">
+        <h2 class="section-heading">ABOUT US</h2>
+        <img src="assets/img/logo/repute-logo-nav-slategray.png">
+        <p class="lead">Getting to know us better</p>
+        <hr>
+      </div>
+      <p>{{ msg }}</p>
+    </div>
 </template>
 
 <script>
@@ -31,30 +22,5 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
