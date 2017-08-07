91058a5b068200fea13bf4365c72110c7749cbd6
davidhope <flav.flavor@gmail.com>
2017-08-07 14:01:25 -0500

    updating wd with branch

---
+++ src/components/dlh-back-to-top.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="back-to-top">
+		<a href="#top"><i class="fa fa-chevron-up"></i></a>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'BackToTop'
+}
+</script>
+
+<style lang="scss"></style>
